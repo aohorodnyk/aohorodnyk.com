980020ebb57dd61020aad30d63303ae1ccf9b0a3
Anton Ohorodnyk <anton@ohorodnyk.name>
2021-01-03 23:11:14 -0800

    Change branch name to aohorodnyk

--- themes/sync.sh
+++ themes/sync.sh
@@ -2,7 +2,7 @@
 
 THEME="hugo-future-imperfect-slim"
 rm -rf ./${THEME}
-git clone git@github.com:aohorodnyk/hugo-future-imperfect-slim.git --branch simple-toc
+git clone git@github.com:aohorodnyk/hugo-future-imperfect-slim.git --branch aohorodnyk
 rm -rf ${THEME}/.git
 git add -- ./${THEME}/
 git commit -m"Update ${THEME} theme" -- ./${THEME}
