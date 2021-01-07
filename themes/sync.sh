5146752bb5f8b09cfb023d5ef7a96a3129c69a58
Anton Ohorodnyk <anton@ohorodnyk.name>
2021-01-06 23:34:28 -0800

    Update in symc.sh

--- themes/sync.sh
+++ themes/sync.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
-THEME="hugo-future-imperfect-slim"
-rm -rf ./${THEME}
-git clone git@github.com:aohorodnyk/hugo-future-imperfect-slim.git --branch aohorodnyk
-rm -rf ${THEME}/.git
-git add -- ./${THEME}/
-git commit -m"Update ${THEME} theme" -- ./${THEME}
+update_theme() {
+  THEME_URL=$1
+  THEME=$(echo ${THEME_URL} | grep -oe "[^\/]\+.git$" | sed "s/.git//g")
+  echo "Updating ${THEME}..."
+
+  rm -rf ./${THEME}
+  git clone $THEME_URL
+  rm -rf ${THEME}/.git
+  git add -- ./${THEME}/
+  git commit -m"Update ${THEME} theme" -- ./${THEME}
+
+  echo "Updated ${THEME}"
+}
+
+update_theme "git@github.com:aohorodnyk/hugo-future-imperfect-slim.git"
